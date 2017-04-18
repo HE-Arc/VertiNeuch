37c87ff0035b6818bd084f1bf3ac4c3b36bf18c8
Nicolas Gonin <thegazou@romandie.com>
2017-04-18 19:59:16 +0200

    suppress mail module

--- config/settings/production.py
+++ config/settings/production.py
@@ -73,13 +73,7 @@ DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL',
 EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[VertiNeuch]')
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
 
-# Anymail with Mailgun
-INSTALLED_APPS += ['anymail', ]
-ANYMAIL = {
-    'MAILGUN_API_KEY': env('DJANGO_MAILGUN_API_KEY'),
-    'MAILGUN_SENDER_DOMAIN': env('MAILGUN_SENDER_DOMAIN')
-}
-EMAIL_BACKEND = 'anymail.backends.mailgun.MailgunBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console'
 
 # TEMPLATE CONFIGURATION
 # ------------------------------------------------------------------------------
